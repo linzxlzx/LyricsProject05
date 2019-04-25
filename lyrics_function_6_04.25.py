c0a5ea83e7d130be191f3ddd3013506d6a14a5f8
ZixuanLin <zl2725@columbia.edu>
2019-04-25 13:21:51 -0400

    Add file for lyrics functions

---
+++ lyrics_function_6_04.25.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 12:27:52 2019
+
+@author: zixuan_leah
+"""
+
+from load_v8 import *
+# lyrics = read_lyrics("Lyrics.zip")
+# details = get_details(names_list)
+
+#names_list, song_lyrics = read_lyrics(lyric_path)
+#song_details = get_details(names_list, song_lyrics)
+
+# names_list = []  # the list of song names from zip file
+# song_lyrics = {}  # txtname as key, lyrics lists as value
+# song_details = {}  # txtname as key, song details as value
+
+love_dic = {}  # what we will return in love method: song as key, rate as value
+kidsafe_dic = {}  # what we will retrun in kidsafe method
+length_dic = {}
+sent_dic = {}
+com_dic = {}
+#print(a)
+#print(names_list)
+
+
+#define a function to save a list of positive and negative word
+def get_pos_neg_words():
+    def get_words(url):
+        import requests
+        words = requests.get(url).content.decode('latin-1')
+        word_list = words.split('\n')
+        index = 0
+        while index < len(word_list):
+            word = word_list[index]
+            if ';' in word or not word:
+                word_list.pop(index)
+            else:
+                index+=1
+        return word_list
+
+    p_url = 'http://ptrckprry.com/course/ssd/data/positive-words.txt'
+    n_url = 'http://ptrckprry.com/course/ssd/data/negative-words.txt'
+    positive_words = get_words(p_url)
+    negative_words = get_words(n_url)
+    return positive_words,negative_words
+
+
+#define a function to return a list of the percentage of positive word and negative word for each song
+def sentiment(songnames: list):
+    positive_words,negative_words = get_pos_neg_words()
+    results = list()
+    for song in songnames:
+        cpos = cneg = 0
+        for word in song_lyrics[song]:
+            if word in positive_words:
+                cpos+=1
+            if word in negative_words:
+                cneg+=1
+        results.append((song,cpos/len(song_lyrics.get(song))-cneg/len(song_lyrics.get(song))))
+        result={song[0]:song[1] for song in results}
+    return result
+
+
+
+
+
+
+def love(songnames: list, lovelist: list) -> dict:
+    lo_dic = {}    
+    for name in songnames:
+        match_love = 0
+        lovetext = song_lyrics[name]
+        for word in lovetext:
+            if word in lovelist:
+                match_love += 1
+        love_rate = match_love / len(lovetext)
+        lo_dic[name] = love_rate
+    return lo_dic
+
+
+def kid_safe(songnames: list, dirtylist: list, sent_rsl: dict) -> dict:
+    k_dic = {}
+    for name in songnames:
+        match_dirty = 0
+        dirtytext = song_lyrics[name]
+        for word in dirtytext:
+            if (word in dirtylist) or ("*" in word):
+                match_dirty += 1
+        dirty_rate = match_dirty / len(dirtytext)
+        sent_rate = sent_rsl[name]
+        kidsafe_rate = 0.7 * (1 - dirty_rate) + 0.3 * sent_rate
+        k_dic[name] = kidsafe_rate
+    return k_dic
+#print(kid_safe(names_list,"dirty_words.txt"))
+
+
+def length(songnames: list) -> dict:
+    len_dic = {}
+    for name in songnames:
+        length = len(song_lyrics[name])
+        len_dic[name] = length
+    return len_dic
+#print(length(names_list))
+    
+def complexity(songnames: list) -> dict:
+    com_dic = {}
+    for name in songnames:
+        com = len(set(song_lyrics[name]))
+        com_dic[name] = com
+    return com_dic
+# =============================================================================
+# re here
+# =============================================================================
+def read_txt(filename):
+    with open(filename, "r") as file:
+        wl_1 = file.readlines()
+        wl = []
+        for words in wl_1:
+            wl.append(words.strip("\n"))
+    return wl
+#print(read_txt("love_words.txt"))
+    
+
+
+
+# =============================================================================
+# ranking
+# =============================================================================
+    
+
+#separate the result into two list with sorted rank
+def result_rank(sentiment, love, kid_safe, length, complexity):
+    result_rank_sen = sentiment
+    result_rank_love = love
+    result_rank_kid = kid_safe
+    result_rank_len = length
+    result_rank_comp = complexity
+    import operator
+    sorted_rank_sen = dict(sorted(result_rank_sen.items(),
+                                  key=operator.itemgetter(1), reverse=True))
+    sorted_rank_love = dict(sorted(result_rank_love.items(),
+                                   key=operator.itemgetter(1), reverse=True))
+    sorted_rank_kid = dict(sorted(result_rank_kid.items(),
+                                  key=operator.itemgetter(1), reverse=True))
+    sorted_rank_len = dict(sorted(result_rank_len.items(),
+                                  key=operator.itemgetter(1), reverse=True))
+    sorted_rank_comp = dict(sorted(result_rank_comp.items(),
+                                   key=operator.itemgetter(1), reverse=True))
+    import pandas as pd
+    results = pd.DataFrame.from_dict([sorted_rank_sen, sorted_rank_love,
+                                      sorted_rank_kid, sorted_rank_len,
+                                      sorted_rank_comp]).T
+    results.columns = ['mood', 'love', 'kid_safe', 'length', 'complexity']
+    results['mood'] = pd.qcut(results['mood'], 11,
+                              labels=False, duplicates='drop')
+    results['love'] = pd.qcut(results['love'], 11,
+                              labels=False, duplicates='drop')
+    results['kid_safe'] = pd.qcut(results['kid_safe'], 11, labels=False,
+                                  precision=100, duplicates='drop')
+    results['length'] = pd.qcut(results['length'], 11,
+                                labels=False, duplicates='drop')
+    results['complexity'] = pd.qcut(results['complexity'], 11,
+                                    labels=False, duplicates='drop')
+    return results
+
+
+
+
+
+
+
+love_l = read_txt("love_words.txt")
+dir_l = read_txt("dirty_words.txt")
+sent_dic = sentiment(names_list)
+kidsafe_dic = kid_safe(names_list, dir_l, sent_dic)
+#print(kidsafe_dic)
+love_dic = love(names_list, love_l)
+length_dic = length(names_list)
+
+com_dic = complexity(names_list)
+
+#print(kidsafe_dic)
+
+#print("123")
+finalresult=result_rank(sent_dic, love_dic, kidsafe_dic, length_dic, com_dic).T.to_dict()
+#print(finalresult)
+##finalresult.update(song_template)
+import json
+dump = json.dump(finalresult, sys.stdout, indent = 4)
+#print(dump)
+#loaded_results = json.loads(dump)
+#print(loaded_results)
