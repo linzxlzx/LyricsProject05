8bee9bb39891f616ca6ca24aa039dc0d7fa866e2
ZixuanLin <zl2725@columbia.edu>
2019-05-02 12:44:36 -0400

    Make changes to main function

--- main.py
+++ main.py
@@ -6,10 +6,29 @@ Created on Thu Apr 25 13:22:41 2019
 @author: zixuan_leah
 """
 
-from lyrics_function import *
-from load import *
+import lyricsfunc
+import load
 import json
-import pandas as pd
+import sys
+
+
+def main(path = None):
+    names_buffer = load.read_path(path)
+    names_list, song_lyrics = load.read_lyrics(names_buffer)
+    song_details = load.get_details(names_list, song_lyrics)
+    love_l = lyricsfunc.read_txt("love_words.txt")
+    notsafe_l = lyricsfunc.read_txt("not_kidsafe.txt")
+    sent_dic = lyricsfunc.sentiment(names_list, song_lyrics)
+    kidsafe_dic = lyricsfunc.kid_safe(names_list, notsafe_l, sent_dic,song_lyrics)
+    love_dic = lyricsfunc.love(names_list, love_l,song_lyrics)
+    length_dic = lyricsfunc.length(names_list,song_lyrics)
+    com_dic = lyricsfunc.complexity(names_list,song_lyrics)
+    sentdic, lovedic, kidsafedic, lengthdic, comdic = lyricsfunc.result_rank(sent_dic, love_dic, kidsafe_dic, length_dic, com_dic,song_details)
+    result = lyricsfunc.get_finaldic(sentdic, lovedic, kidsafedic, lengthdic, comdic, song_details,names_list)
+    finalresult = {}
+    finalresult["characterizations"] = result
+    json.dump(finalresult, sys.stdout, indent=4)
+
 
 if __name__ == "__main__":
     import argparse
@@ -18,29 +37,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     lyric_path = args.lyrics_path
-#    lyric_path = "Lyrics"
-
-    names_list, song_lyrics = read_lyrics(lyric_path)
-    song_details = get_details(names_list, song_lyrics)
-#    
-    love_l = read_txt("love_words.txt")
-    notsafe_l = read_txt("not_kidsafe.txt")
-    sent_dic = sentiment(names_list, song_lyrics)
-    kidsafe_dic = kid_safe(names_list, notsafe_l, sent_dic,song_lyrics)
-#    #print(kidsafe_dic)
-    love_dic = love(names_list, love_l,song_lyrics)
-    length_dic = length(names_list,song_lyrics)
-#    
-    com_dic = complexity(names_list,song_lyrics)
-#    
-    
-#    import pandas as pd
-    result_0 = create_song_details_dic(song_details).join(result_rank(sent_dic, love_dic, kidsafe_dic, length_dic, com_dic))
-    result_1 = list()
-    for song in names_list:
-        result_1.append(result_0.T.to_dict()[song])
-        
-    finalresult={"characterizations":result_1}
-#    finalresult=result_rank(sent_dic, love_dic, kidsafe_dic, length_dic, com_dic).T.to_dict()
-#
-    json.dump(finalresult, sys.stdout, indent = 4)
+#     lyric_path = "Lyrics"    
+    main(lyric_path)
