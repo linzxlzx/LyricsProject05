8f16b8fe1b798db4932713ee0e5db5db217dbd18
tiang13 <gt2408@columbia.edu>
2019-05-05 15:46:32 -0400

    Update the main file

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ import json
 import sys
 
 
-def main(path = None):
+def main(path=None):
     names_buffer = load.read_path(path)
     names_list, song_lyrics = load.read_lyrics(names_buffer)
     song_details = load.get_details(names_list, song_lyrics)
     love_l = lyricsfunc.read_txt("love_words.txt")
     notsafe_l = lyricsfunc.read_txt("not_kidsafe.txt")
-    sent_dic = lyricsfunc.sentiment(names_list, song_lyrics)
-    kidsafe_dic = lyricsfunc.kid_safe(names_list, notsafe_l, sent_dic,song_lyrics)
-    love_dic = lyricsfunc.love(names_list, love_l,song_lyrics)
-    length_dic = lyricsfunc.length(names_list,song_lyrics)
-    com_dic = lyricsfunc.complexity(names_list,song_lyrics)
-    sentdic, lovedic, kidsafedic, lengthdic, comdic = lyricsfunc.result_rank(sent_dic, love_dic, kidsafe_dic, length_dic, com_dic,song_details)
-    result = lyricsfunc.get_finaldic(sentdic, lovedic, kidsafedic, lengthdic, comdic, song_details,names_list)
+    sent_dic = lyricsfunc.mood(names_list, song_lyrics)
+    kidsafe_dic = lyricsfunc.kid_safe(
+        names_list, notsafe_l, sent_dic, song_lyrics)
+    love_dic = lyricsfunc.love(names_list, love_l, song_lyrics)
+    length_dic = lyricsfunc.length(names_list, song_lyrics)
+    com_dic = lyricsfunc.complexity(names_list, song_lyrics)
+    sentdic, lovedic, kidsafedic, lengthdic, comdic = lyricsfunc.result_rank(
+        sent_dic, love_dic, kidsafe_dic, length_dic, com_dic, song_details)
+    result = lyricsfunc.get_finaldic(
+        sentdic, lovedic, kidsafedic, lengthdic,
+        comdic, song_details, names_list)
     finalresult = {}
     finalresult["characterizations"] = result
     json.dump(finalresult, sys.stdout, indent=4)
@@ -36,6 +40,5 @@ if __name__ == "__main__":
     parser.add_argument('lyrics_path', help='Path to the Lyrcis folder')
     args = parser.parse_args()
 
-    lyric_path = args.lyrics_path
-#     lyric_path = "Lyrics"    
+    lyric_path = args.lyrics_path  
     main(lyric_path)
