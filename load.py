3f7d2e5885bfda53784d01913cb6e9994993a074
ZixuanLin <zl2725@columbia.edu>
2019-05-05 15:07:27 -0400

    Change functions in load

--- load.py
+++ load.py
@@ -6,49 +6,56 @@ Created on Fri Mar 29 14:42:39 2019
 @author: zixuan_leah
 """
 
-import sys
 import os
 import nltk
-#nltk.download('wordnet')
-#nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet
+nltk.download('wordnet')
+nltk.download('stopwords')
 
 
-# return the songname and lyric words as a dic
-def read_lyrics(path: str) -> dict:
-    lyrics = {}
+def read_path(path: str) -> dict:
     names = os.listdir(path)
+    buffer = {}
     for songtxt in names:
-        with open(path + "/" + songtxt) as lyrics_context:         
+        if songtxt.endswith(".txt"):
+            with open(path + "/" + songtxt, encoding="utf8") as lyrics_context:
+                buffer[songtxt] = lyrics_context.readlines()
+    return buffer
+
+
+def read_lyrics(filedict: dict) -> (list, dict):
+    names = list(filedict.keys())
+    lyrics = {}
+    for song in names:
             text = ''
-            for sentence in lyrics_context.readlines():  # split into sentences
+            for sentence in filedict[song]:  # split into sentences
                 text += sentence.strip("\r\n")+" "
-    
             words = text.split(" ")  # split into words as a list
             word_list = []
             for letters in words:  # take off chars "...", ","...
                 word_adj = ''.join([char.lower() for char in letters
                                     if (char.isalpha()
-                                        or char == "'"  # "it's" as one word
-                                        or char == "*"
-                                        or char == "-")])
+                                        or char == "'"  # "it's" as word
+                                        or char == "*"  # "f**k" as word
+                                        or char == "-")])  # "o-o-oh" as word
                 if not (word_adj == '' or word_adj == "-" or word_adj == "'"):
                     if "-" in word_adj:  # split "Rang-dang-digidy-dang-a-dang"
                         sub_words = word_adj.split("-")
                         word_list.extend(sub_words)
                     else:
                         word_list.append(word_adj)
-            lyrics[songtxt] = word_list  # put songname and lyrics in dic
 
-    return names, lyrics  # return a dic
+            lyrics[song] = word_list  # txtname as key, lyric wordlist as value
+
+    return names, lyrics  # return songnames list and lyrics dictinary
 
 
 # func used to detct whether is english song
 def detect_lang(songwords: list) -> bool:
+    from nltk.corpus import stopwords
+    from nltk.corpus import wordnet
     index = 0
     count = 1
-    stop = 10  # we take the first 10 words to detct whether it is english
+    stop = 20  # we take the first 20 words to detct whether it is english
     eng_words = 0
     words_chosen = []
     stop_words = set(stopwords.words('english'))
@@ -63,14 +70,17 @@ def detect_lang(songwords: list) -> bool:
                     eng_words += 1
                 count += 1
         index += 1
+    if count < stop:
+        stop = count
 # we set the criteria: English song has 70% of first 10 words as English
+# take into consideration of slangs like 'yo', 'umm'...
     if eng_words / stop >= 0.7:
         return True
     else:
         return False
 
 
-# value: [id, artist, name, T/F]
+# func to store song name, id, artist and English detection result
 def get_details(namelist: list, lyricdic: dict) -> dict:
     song_details = {}
     for txtname in namelist:
@@ -81,4 +91,4 @@ def get_details(namelist: list, lyricdic: dict) -> dict:
             detect_res = detect_lang(lyricdic[txtname])
         song_de.append(detect_res)  # also append the language result
         song_details[txtname] = song_de
-    return song_details
+    return song_details  # key: txtname, value: [id, artist, name, T/F]
